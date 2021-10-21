83ae70dac9a3c1ab54ef577bbfadbd30d1196d82
EtashDutta <77791852+EtashDutta@users.noreply.github.com>
2021-10-21 19:53:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function addUser(){
+    p1=document.getElementById("p1_text_input").value;
+    p2=document.getElementById("p2_text_input").value;
+
+    localStorage.setItem("p1",p1);
+    localStorage.setItem("p2",p2);
+
+}
